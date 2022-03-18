89fdf7c0ce59c0bd6d54a40933f87562c1005fbe
Ice Lee <lb291700351@live.cn>
2022-03-18 11:19:51 +0800

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ plugins {
 }
 
 rootProject.group = "io.github.291700351"
-rootProject.version = "1.0.1"
-rootProject.description = "预发布1.0.1版本"
+rootProject.version = "1.0.2"
+rootProject.description = "添加core-common"
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
